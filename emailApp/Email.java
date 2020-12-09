1929382d4c70ffc06a6e0550dad4dfef0d3767de
Vijaykottaiyan K <vijayk1409vj@gmail.com>
2020-12-09 22:19:46 +0530

    setDepartment() method implemented

--- emailApp/Email.java
+++ emailApp/Email.java
@@ -1,5 +1,7 @@
 package emailApp;
 
+import java.util.*;
+
 public class Email {
     private String firstName, lastName;
     private String department;
@@ -13,9 +15,25 @@ public class Email {
         this.lastName = lastName;
         System.out.println("Welcome to "+companyName+", "+this.firstName+" "+this.lastName);
 
+        department = setDepartment();
     }
     
     private String setDepartment() {
-
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Select your Department \n 1 - Sales \n 2 - Developement \n 3 - Accounting \n 0 - None");
+        int choice = scn.nextInt();
+        
+        if(choice == 1){
+            return "sales";
+        }
+        else if(choice == 2){
+            return "dev";
+        }
+        else if(choice == 3){
+            return "accnts";
+        }
+        else{
+            return " ";
+        }
     }
 }
--- emailApp/EmailApp.java
+++ emailApp/EmailApp.java
@@ -6,10 +6,11 @@ class EmailApp{
     public static void main(String args[]){
         Scanner scn= new Scanner(System.in);
         String firstName, lastName;
-        System.out.println("\nEnter your first name : ");
+        System.out.print("Enter your first name : ");
         firstName = scn.nextLine();
-        System.out.println("\nEnter your last name :");
+        System.out.print("\nEnter your last name : ");
         lastName = scn.nextLine();
+        System.out.println();
         Email email = new Email(firstName, lastName);
         scn.close();
     }
